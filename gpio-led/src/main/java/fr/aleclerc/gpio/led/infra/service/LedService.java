5a6a5f44170d0b7c91a8e4492cf1f3d6772e3001
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-01 20:25:43 +0200

    ws

---
+++ gpio-led/src/main/java/fr/aleclerc/gpio/led/infra/service/LedService.java
@@ -0,0 +1,7 @@
+package fr.aleclerc.gpio.led.infra.service;
+
+public class LedService {
+	public void clignote(){
+		
+	}
+}
---
+++ gpio-led/src/main/java/fr/aleclerc/gpio/led/ws/service/LedRessource.java
@@ -0,0 +1,30 @@
+package fr.aleclerc.gpio.led.ws.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+@Path("led")
+public class LedRessource {
+	
+	
+    @GET
+    @Path("clignote")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        final GpioController gpio = GpioFactory.getInstance();
+        final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin (
+                RaspiPin.GPIO_01,
+                "LED1",
+                PinState.LOW);
+        led1.blink(500, 10000);
+        return "Got it!";
+    }
+}
