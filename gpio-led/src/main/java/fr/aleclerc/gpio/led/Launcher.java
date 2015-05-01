043722e971d8d89835ab9b5eec16c7ad435010e7
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-01 14:44:31 +0200

    init

---
+++ gpio-led/src/main/java/fr/aleclerc/gpio/led/Launcher.java
@@ -0,0 +1,27 @@
+package fr.aleclerc.gpio.led;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+
+public class Launcher {
+	   public static void main(String[] args) {
+	        System.out.println("Hello, Raspberry Pi!");
+	 
+	        /* Initialize pi4j */
+	        final GpioController gpio = GpioFactory.getInstance();
+	         
+	        
+	        final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin (
+	                RaspiPin.GPIO_01,
+	                "LED1",
+	                PinState.LOW);
+	        led1.pulse(1000);
+	      
+	 
+	        /* Close all open connections. */
+	        gpio.shutdown();
+	    }
+}
