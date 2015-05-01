5ed29346ecc53941f667caf8b7795d12ebb379ee
Amaury LECLERC <amauryleclerc@hotmail.fr>
2015-05-01 15:41:32 +0200

    thread

--- gpio-led/src/main/java/fr/aleclerc/gpio/led/Launcher.java
+++ gpio-led/src/main/java/fr/aleclerc/gpio/led/Launcher.java
@@ -12,16 +12,57 @@ public class Launcher {
 	 
 	        /* Initialize pi4j */
 	        final GpioController gpio = GpioFactory.getInstance();
-	         
+	        boolean BLOCKING = true;
+	        boolean NONBLOCKING = false;
+	 
+	        System.out.println("===== Starting Tutorial 2 =====");
 	        
 	        final GpioPinDigitalOutput led1 = gpio.provisionDigitalOutputPin (
 	                RaspiPin.GPIO_01,
 	                "LED1",
 	                PinState.LOW);
-	     //   led1.pulse(1000, true);
-	        led1.blink(500, 10000); 
+	        /*
+	         * Add a shutdown hook so that the application can trap a Ctrl-C and
+	         * handle it gracefully by ensuring that all the LEDs are turned off
+	         * prior to exiting.
+	         */
+	        Runtime.getRuntime().addShutdownHook(new Thread() {
+	            @Override
+	            public void run() {
+	                System.out.println("\n\nPROGRAM WAS INTERRUPTED. SHUTTING " +
+	                        "DOWN!");
+	                led1.low();
+	                gpio.shutdown();
+	            }
+	        });
 	 
-	        /* Close all open connections. */
-	        gpio.shutdown();
+	        System.out.println("Pins initialized. All LEDs should be off.");
+	 
+	        /*
+	         * Starting the main loop of our program. We put it in a try-catch
+	         * since the various sleep's throw an exception and we need to catch
+	          * those exceptions.
+	         */
+	        try {
+	            System.out.println("Blocking Call: Each LED will be on for 1 sec" +
+	                    " one after another.");
+	            Thread.sleep(1000);
+	 
+	            /*
+	             * Blocking call: Execution proceeds to next line only after current
+	             * line is finished. LEDs will blink one after the other.
+	             */
+	            led1.blink(500, 10000);
+	 
+	         
+	            Thread.sleep(10000);
+	 
+	          
+	        } catch (InterruptedException ie) {
+	            System.out.println("Execution Interrupted.");
+	            led1.low();
+	            gpio.shutdown();
+	        }
 	    }
+	    
 }
